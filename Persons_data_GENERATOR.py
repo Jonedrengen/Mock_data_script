e357b7a45f5577c35c9807b2396e06e0f65b278b
Jon Slotved <jonsslotved@gmail.com>
2023-10-20 14:33:38 +0200

    A lot of changes happened

--- Persons_data_GENERATOR.py
+++ Persons_data_GENERATOR.py
@@ -1,60 +1,162 @@
-# creating a tuple of data, containing a cpr-number, a gender based on the cpr-number, a region, and a phone number
+# code explanation always above and/or next to the code
 
-#importing the classes from the providers
-from CPR_provider import CPR_provider
-from Gender_provider import gender
-from Region_provider import Region_provider
-from PhoneNumber_provider import Phone_number
-
-#importing the csv module
+# import the Faker class from the faker module
+from faker import Faker 
+# importing the BaseProvider class will enable us to create custom providers
+from faker.providers import BaseProvider
+# importing the random module will enable us to use the random.choice() method
+import random
+# for creating csv files
 import csv
 
-# importing the Faker class from the faker module
+# creating instance of Faker class (so I can use the methods of the class)
+fake = Faker()
+
+#_______________________________________________________________________________
+#_______________________________________________________________________________
+#_______________________________________________________________________________
+
+# CPR
+
+# import the Faker class from the faker module
+from faker import Faker 
+# importing the BaseProvider class will enable us to create custom providers
+from faker.providers import BaseProvider
+# importing the random module will enable us to use the random.choice() method
+import random
+
+class CPR_provider(BaseProvider): # -> CPRprovider class inherits from BaseProvider, which is a class from the faker module, that enables us to create custom providers
+    def cpr(self): # -> we create a method (function inside a class) called cpr() that takes in the self parameter.
+        
+        # creating a date of birth (dob) for the first 6 digits of the CPR number
+        # tzinfo refers to the timezone information, which we don't need in this case
+        # generator is a method from the faker module that generates random data, in this case a date of birth
+        dob = self.generator.date_of_birth(tzinfo=None, minimum_age=18, maximum_age=95).strftime("%d%m%y") # -> strftime() is a method from that formats the date of birth to the format we want, in this case dd/mm/yy
+
+        # creating the last 4 digits of the CPR number, where the first represents the gender
+        gender_digit = int
+        
+        # should be easy to read, but if not: if random.choice chooses True, then it will choose a random number from the list [0, 2, 4, 6, 8], else it will choose a random number from the list [1, 3, 5, 7, 9]
+        if random.choice([True, False]) == True: 
+            gender_digit = random.choice([0, 2, 4, 6, 8])
+        else:
+            gender_digit = random.choice([1, 3, 5, 7, 9])
+
+        #then create the last 3 digits of the CPR number with the random.randint() method
+        unique_digits = random.randint(100, 999)
+
+        # I then create the CPR by combining these 3 variables
+        CPR_number = f"{dob}{gender_digit}{unique_digits}"
+        
+        # then I return the CPR number
+        return str(CPR_number)
+    
+# ________TO USE THE CPRprovider CLASS________
+# we create an instance of the Faker class, initializing the faker generator
+# -> fake = Faker()
+
+# we then test the CPRprovider class by creating an instance of it
+# -> fake.add_provider(CPR_provider)
+
+# we can print a single CPR number by calling the cpr() method
+# -> print(fake.cpr())
+
+# we can also print multiple CPR numbers by using a for loop
+# -> for ele in range(10):
+# ->     print(fake.cpr())
+
+
+#_______________________________________________________________________________
+#_______________________________________________________________________________
+#_______________________________________________________________________________
+
+# Gender
+
+# import the Faker class from the faker module
+from faker import Faker
+
+# importing the BaseProvider class will enable us to create custom providers
+class gender:
+    # writing @staticmethod above the method, will make it a static method, which means that it can be called without creating an instance of the class
+    # you call it by writing the class name, followed by a dot, and then the method name (see line 31 in Persons_data_GENERATOR.py)
+    @staticmethod
+    def gender_determination(CPR_number):
+        # checks the 10'th digit of the CPR number, since that determines the gender of the person
+        gender_digit = int(CPR_number[9])
+
+     # this determines whether the gender digit is even or odd. If the 
+        if gender_digit % 2 == 0:
+            return "F"
+        else:
+            return "M"
+        
+# # ________TO USE THE gender CLASS________ 
+# # IS DEPENDANT ON THE CPRprovider CLASS!
+
+# # we create an instance of the Faker class, initializing the faker generator
+# # -> fake = Faker()
+
+# # we then test the CPR_provider class by creating an instance of it
+# # cpr_provider_instance = CPR_provider(fake)
+
+# # we can then call the gender_determination() method on the cpr_provider_instance
+# # gender.gender_determination(cpr_provider_instance.cpr())
+
+
+#_______________________________________________________________________________
+#_______________________________________________________________________________
+#_______________________________________________________________________________
+
+# Phone number (danish Phone numbers only)
+
+# import random module
+import random
+
+class Phone_number:
+    @staticmethod
+    def number():
+        number = random.randint(10000000, 99999999)
+        return str(number)
+    
+#________TO USE THE Phone_number CLASS________
+
+#testing the class
+# -> result = Phone_number()
+
+# printing a single phone number
+# -> print(result.number())
+
+# printing multiple phone numbers
+# -> for ele in range(10):
+# ->    print(result.number())
+
+
+#_______________________________________________________________________________
+#_______________________________________________________________________________
+#_______________________________________________________________________________
+
+# Region
+
+# import the Faker class from the faker module
 from faker import Faker
 # importing the BaseProvider class will enable us to create custom providers
 from faker.providers import BaseProvider
 
-#creating an instance of the Faker class
-fake = Faker()
+class Region_provider(BaseProvider):
+    def region(self):
+        # creating the list of regions
+        regions = ["Hovedstaden", "Sjælland", "Syddanmark", "Midtjylland", "Nordjylland"]
+        # using the random_element() method to choose a random region from the list
+        region = self.random_element(regions)
+        return str(region)
 
-#creating instances of the classes, so I can use the methods from the classes
-CPR_provider_instance = CPR_provider(fake)
-#does not need an instance of faker class, because gender is determined based on the provided cpr number
-Gender_provider_instance = gender()
-Region_provider_instance = Region_provider(fake)
-#does not need an instance of faker class, because the random module is used to get a random phone number
-PhoneNumber_provider_instance = Phone_number()
+# ________TO USE THE Region_provider CLASS________
 
+# we create an instance of the Faker class, initializing the faker generator
+# -> fake = Faker()
 
-def Persons_data_GENERATOR():
-    # Generating random data for the persons table
-    cpr = CPR_provider_instance.cpr()
-    Gender = Gender_provider_instance.gender_determination(cpr) # <- determines gender based on the cpr number (7th number)
-    Region = Region_provider_instance.region()
-    Phone_number = PhoneNumber_provider_instance.number()
+# we then test the Region_provider class by creating an instance of it
+# -> fake.add_provider(Region_provider)
 
-    #generating a tuple of data
-    Persons_tuple = (cpr, Gender, Region, Phone_number)
-    return Persons_tuple
-
-def create_csv_file():
-    #to create a csv file, we need to open the file in write mode
-    with open("Persons_data.csv", "w", newline="") as csvfile:
-        #to create a csv file, we need to create a writer object
-        writer = csv.writer(csvfile)
-        # we then need to write the header row
-        writer.writerow(["CPR", "Gender", "Region", "Phone_number"])
-        # we then create 200 rows of data
-        for ele in range(200):
-            # we then call the Persons_data_GENERATOR() function to generate the data
-            row_data = Persons_data_GENERATOR()
-            #then we write the data to the csv file
-            writer.writerow(row_data)
-
-# Call the create csv function to create the csv file
-create_csv_file()
-
-# #testing the Persons_data_GENERATOR() function
-# for ele in range(10):
-#     print(Persons_data_GENERATOR())
-
+# we can print a single region by calling the region() method
+# -> print(fake.region())
