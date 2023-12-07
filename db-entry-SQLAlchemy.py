1e233d13628bc4534accfa6f1f40f02292ee6622
Jon Slotved <127400053+Jonedrengen@users.noreply.github.com>
2023-12-07 10:42:51 +0100

    split the fastapi application and the mock data script

--- db-entry-SQLAlchemy.py
+++
@@ -1,8 +0,0 @@
-from database_SQLAlchemy_calls import get_all_Persons, get_all_Sample, get_all_S_aureus, get_all_COVID19, get_all_Batch, get_all_Legionella, get_all_S_epidermidis, get_all_SequencedSample
-
-def main():                         
-    result = get_all_SequencedSample()
-    print(result[:10])
-
-if __name__ == main():
-    main()
